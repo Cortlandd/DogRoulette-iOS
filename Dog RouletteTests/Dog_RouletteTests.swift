c194a7065923d7352b954daf28e601957570a1b8
Cortland Walker <cortland1568@gmail.com>
2018-06-04 19:40:50 -0500

    Initial Commit

---
+++ Dog RouletteTests/Dog_RouletteTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dog_RouletteTests.swift
+//  Dog RouletteTests
+//
+//  Created by Cortland on 6/4/18.
+//  Copyright © 2018 Cortland Walker. All rights reserved.
+//
+
+import XCTest
+@testable import Dog_Roulette
+
+class Dog_RouletteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
